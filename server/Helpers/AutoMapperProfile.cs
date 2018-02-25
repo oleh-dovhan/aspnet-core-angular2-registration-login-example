2f39c1a42015ae697ff70f80a3c91a0af028a1d6
Oleh Dovhan <oleh.dovhan.731@outlook.com>
2018-02-25 17:02:16 +0200

    Initial commit

---
+++ server/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using server.Entities;
+using server.Dtos;
+
+namespace server.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
---
+++ server/Helpers/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using server.Entities;
+
+namespace server.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
